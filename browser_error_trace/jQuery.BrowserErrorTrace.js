342a0e2d12ffefb3e47013a1ad7b794479223019
narumi <seawave.works@gmail.com>
2015-03-02 13:18:36 +0900

    更新しました。 errorをサーバーにgetで送信します。

--- browser_error_trace/jQuery.BrowserErrorTrace.js
+++ browser_error_trace/jQuery.BrowserErrorTrace.js
@@ -9,26 +9,42 @@
  * @modified  : 2015-02-26 16:38:00
  */
 ;(function($) {
-	
+	//初期設定
+	var defaults = { 
+						"url"  : 'http://'
+				   };
+	//設定、初期設定＋追加、上書きされる。
+	var setting = {};			   
+	var ErrorTrace = {
+		"error_fuc":function(message,file,line){
+    		var error_obj = {};
+    		for(var i=0; i < arguments.length; i++){
+    			console.log(arguments["0"]);
+    			//message, file, line, col, errorが入る可能性がある。
+    			if(i===0){ error_obj.message = arguments["0"]; }
+    			if(i===1){ error_obj.file    = arguments["1"]; }
+    			if(i===2){ error_obj.line    = arguments["2"]; }
+    			if(i===3){ error_obj.col     = arguments["3"]; }
+    			if(i===4){ error_obj.error   = arguments["4"]; }
+    		}
+    		
+    		var getParamStr = $.param(error_obj);
+    		new Image().src = setting+"?"+getParamStr;
+    		
+    	}   
+	};
 	$.fn.BrowserErrorTrace = {
-		//初期設定
-		"defaults":{
-	            text   : 'This is basic plugin!!!'
-	    },
-	    //setting
-	    "setting":{},
 		"init":function(options){
 			
 	        //引数を設定する
-	        this.setting = $.extend(this.defaults, options);
-	        window.onerror = this.error_fuc;
+	        setting = $.extend(defaults, options);
+	        window.onerror = ErrorTrace.error_fuc;
 	        return(this);
-    	},
-    	"error_fuc":function(){
-    		console.log(arguments);
     	}
 	};
 	
 	
+				
+			
 	
 })(jQuery);
